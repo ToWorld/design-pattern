642d78ab6faea21e595491235b451dd317363767
JayZhu <jayzhu1215@gmail.com>
2017-02-12 03:08:04 -0800

    first commit

---
+++ TemplateMethod.cc
@@ -0,0 +1,32 @@
+class Library {
+public:
+	// template method
+	void run() {	
+		Step1();
+
+		if (Step2()) {
+			Step3();
+		}
+	
+		for (int i = 0; i < 4; i++) {
+			Step4();
+		}
+
+		Step5();
+	}
+	virtual ~Library() { }
+protected:
+	void Step1() {
+		// TODO
+	}
+	void Step3() {
+		// TODO
+	}
+	void Step5() {
+		// TODO
+	}
+protected:
+	virtual bool Step2() = 0;
+	virtual void Step4() = 0;
+};
+
